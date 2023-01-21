ec27daadaa4354a472d6c9c1f6debb25ed2856b8
cjones1047 <cjmj1047@gmail.com>
2023-01-21 11:43:20 -0600

    List of song titles created using list comprehension; Multiple classes passed to class_ kwarg via song_classes list

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def get_billboard_100(this_date):
     billboard_100_response = requests.get(f"https://www.billboard.com/charts/hot-100/{this_date}/")
     billboard_100_response.raise_for_status()
     billboard_100_page = BeautifulSoup(billboard_100_response.text, "html.parser")
-    print(billboard_100_page)
+    song_classes = [("c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet "
+                     "lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis "
+                     "u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet"),
+                    ("c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet "
+                     "lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis "
+                     "u-max-width-330 u-max-width-230@tablet-only")]
+    song_element_list = billboard_100_page.find_all(name="h3", class_=song_classes)
+    song_title_list = [song_el.text.replace('\n', '').replace('\t', '') for song_el in song_element_list]
+    print(song_title_list)
+    print(len(song_title_list))
 
 
 chosen_date = input("Which date do you want to travel back to? Type in YYYY-MM-DD format:\n")
